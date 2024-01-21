4e46d54bf291b86fab59c5bd84cec64aff386efe
Chell L. <pumpkinbiscuit@protonmail.com>
2024-01-21 18:45:49 -0500

    added tests

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
 
-  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  unstable = import <nixpkgs-unstable> {};
+  pkgs = import <nixos-23.11> { overlays = [ rust_overlay ]; };
+  unstable = import <nixos-unstable> {};
 
   # rust = pkgs.rust-bin.stable."1.74.0".default.override {
   #   extensions = [ "rust-src" ];
@@ -16,23 +16,14 @@ in
     nativeBuildInputs = with pkgs; [
     gcc
     rust
+    rust-analyzer
     # dep
     openssl
     pkg-config
   ];
 
-  shellHook = ''
-    echo "Gcc $(gcc --version | head -n 1 | awk '{print $3}'),
-    $(rustc --version),
-    $(cargo --version),
-    $(cargo fmt --version),
-    $(cargo clippy --version),
-    $(rust-analyzer --version),
-    "
-  '';
-
   # Certain Rust tools won't work without this
   # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
--- src/main.rs
+++ src/main.rs
@@ -78,3 +78,45 @@ fn main() {
     let per_mili: u64 = (VEC_COUNT as u64 * EL_COUNT as u64) / mark_out as u64;
     println!("for {} * {} vec:\n elements per mili: {}", VEC_COUNT, EL_COUNT, per_mili);
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng; // Trait that provides the random number generation methods
+
+    const EL_COUNT: u64 = 1000000;
+    const VEC_COUNT: u32 = 5;
+
+
+    #[test]
+    fn bench() {
+
+        let mut rng = rand::thread_rng(); // Get a random number generator
+
+        let mut vec_of_vecs: Vec<Vec<f64>> = Vec::new();
+        for _ in 0..VEC_COUNT {
+            let nums_1: Vec<f64> = (0..EL_COUNT).map(|_| rng.gen_range(0.0..100.0)).collect();
+            vec_of_vecs.push(nums_1);
+        }
+
+        let len = EL_COUNT;
+        println!("@ Vec Len: {}", len);
+
+        println!("@ Running...");
+
+        let time_out = std::time::Instant::now();
+        for (index, mut i) in vec_of_vecs.iter_mut().enumerate() {
+            let time_in = std::time::Instant::now();
+
+            merge_sort(&mut i);
+
+            let mark_in = time_in.elapsed().as_millis();
+            println!("@ {} in {} ms.", index, mark_in);
+        }
+        let mark_out = time_out.elapsed().as_millis();
+        let per_mili: u64 = (VEC_COUNT as u64 * EL_COUNT as u64) / mark_out as u64;
+        println!("@ for {} * {} vec:\n@ elements per mili: {}", VEC_COUNT, EL_COUNT, per_mili);
+    }
+}
+
